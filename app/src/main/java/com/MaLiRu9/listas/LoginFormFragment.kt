81fdea58cc6cda991cea4681ef58f23894461b58
MaLiRu9 <lolo_fosils_93@yahoo.es>
2022-06-29 13:55:04 +0200

    feat: cancel and next buttons created

--- app/src/main/java/com/MaLiRu9/listas/LoginFormFragment.kt
+++ app/src/main/java/com/MaLiRu9/listas/LoginFormFragment.kt
@@ -1,9 +1,11 @@
 package com.MaLiRu9.listas
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import androidx.fragment.app.Fragment
 import com.jocnunez.listas.R
 
@@ -14,6 +16,11 @@ class LoginFormFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_login_form, container, false)
+        val view = inflater.inflate(R.layout.fragment_login_form, container, false)
+        val nextButton = view.findViewById<Button>(R.id.nextButton)
+        nextButton.setOnClickListener {
+            Log.d("Debug", "Click on next button")
+        }
+        return view
     }
 }
